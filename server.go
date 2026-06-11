72b6014e5a1bf7d7526133131dcdf148ca44be6e
Tomas Xavier Santos <tom.xaviersantos@gmail.com>
2026-06-11 08:46:51 -0300

    feat: updates app route and creates new handler for healthz route

--- server.go
+++ server.go
@@ -5,10 +5,17 @@ import (
 	"net/http"
 )
 
+func handleHealthz(writer http.ResponseWriter, req *http.Request) {
+	req.Header.Set("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte("OK"))
+}
+
 func main() {
 	const port = "8080"
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz/", handleHealthz)
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
