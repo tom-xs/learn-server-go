e92e04d2ef70aaac873b231fa24a324af80f1f57
Tomas Xavier Santos <tom.xaviersantos@gmail.com>
2026-06-11 08:11:03 -0300

    feat: serves HTml using file server

--- server.go
+++ server.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	const port = "8080"
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
